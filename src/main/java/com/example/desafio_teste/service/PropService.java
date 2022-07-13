1b146a3a1c98e890c9e17f06054b2882813bc2c8
matheusaalves <matheus.aalves@mercadolivre.com>
2022-07-13 17:57:12 -0300

    Adicionando método de calcular área total

--- src/main/java/com/example/desafio_teste/service/PropService.java
+++ src/main/java/com/example/desafio_teste/service/PropService.java
@@ -1,7 +1,23 @@
 package com.example.desafio_teste.service;
 
+import com.example.desafio_teste.model.Prop;
+import com.example.desafio_teste.model.Room;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class PropService implements PropServiceInterface {
+
+    public BigDecimal calculateTotalArea(Prop prop) {
+        RoomService roomService = new RoomService();
+
+         BigDecimal totalArea = prop.getRoomList()
+                                        .stream()
+                                        .map(p -> roomService.calculateArea(p))
+                                        .reduce(BigDecimal.ZERO,
+                                                (sumArea, eachValue) -> sumArea.add(eachValue) );
+
+         return totalArea;
+    }
 }
--- src/main/java/com/example/desafio_teste/service/PropServiceInterface.java
+++ src/main/java/com/example/desafio_teste/service/PropServiceInterface.java
@@ -1,4 +1,9 @@
 package com.example.desafio_teste.service;
 
+import com.example.desafio_teste.model.Prop;
+
+import java.math.BigDecimal;
+
 public interface PropServiceInterface {
+    public BigDecimal calculateTotalArea(Prop prop);
 }
