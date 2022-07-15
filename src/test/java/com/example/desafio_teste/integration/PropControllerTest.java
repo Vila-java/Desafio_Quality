d95fef362bf4920b5f158ef825841ea8f0b78f8b
matheusaalves <matheus.aalves@mercadolivre.com>
2022-07-15 15:32:57 -0300

    Adicionando teste de integração da us0004

--- src/test/java/com/example/desafio_teste/integration/PropControllerTest.java
+++ src/test/java/com/example/desafio_teste/integration/PropControllerTest.java
@@ -1,18 +1,24 @@
 package com.example.desafio_teste.integration;
 
+import com.example.desafio_teste.dto.RoomDetailsDto;
 import com.example.desafio_teste.model.Prop;
+import com.example.desafio_teste.service.RoomService;
 import com.example.desafio_teste.utils.TestUtilsGenerator;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -26,6 +32,7 @@ class PropControllerTest {
     TestRestTemplate testRestTemplate;
 
     @Test
+    @DisplayName("Verifica se o total de metros quadrados por propriedade está correto")
     void calculateTotalArea_returnTotalArea_whenPropExist() {
         Prop prop = TestUtilsGenerator.getByNameWhenExist();
         String url = "http://localhost:" + port + "/prop/calculateArea/" + prop.getName();
@@ -33,7 +40,6 @@ class PropControllerTest {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(TestUtilsGenerator.getTotalAreaProp());
-
     }
 
     @Test
@@ -45,6 +51,22 @@ class PropControllerTest {
     }
 
     @Test
-    void areaPerRoom() {
+    @DisplayName("Verifica se o total de metros quadrados por comodo está correto")
+    void areaPerRoom_returnListRoomNameAndPrice_whenPropExist() {
+        Prop prop = TestUtilsGenerator.getByNameWhenExist();
+        String url = "http://localhost:" + port + "/prop/areaPerRoom/" + prop.getName();
+
+        ResponseEntity<List<RoomDetailsDto>> response = testRestTemplate.exchange(url,
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<RoomDetailsDto>>() {});
+
+        RoomService roomService = new RoomService();
+
+        List<RoomDetailsDto> expectedResponse = prop.getRoomList()
+                .stream()
+                .map(room -> new RoomDetailsDto(room.getName(), roomService.calculateArea(room)))
+                .collect(Collectors.toList());
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expectedResponse);
     }
 }
