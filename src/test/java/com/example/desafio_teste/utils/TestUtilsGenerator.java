5ea9272887fd6390a75b5576c5cfa63161371dbc
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-15 17:57:45 -0300

    Criando documentaçao JavaDoc

--- src/test/java/com/example/desafio_teste/utils/TestUtilsGenerator.java
+++ src/test/java/com/example/desafio_teste/utils/TestUtilsGenerator.java
@@ -8,7 +8,15 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe de métodos comuns para testes.
+ */
 public class TestUtilsGenerator {
+    /**
+     * Chama o nome quando existir
+     *
+     * @return nome quando existir
+     */
     public static Prop getByNameWhenExist() {
         List<Room> roomList = new ArrayList<>();
         roomList.add(new Room("Quarto", 1.5, 2.0));
@@ -21,14 +29,29 @@ public class TestUtilsGenerator {
         return prop;
     }
 
+    /**
+     * Chama a área total da propriedade
+     *
+     * @return área total da propriedade
+     */
     public static BigDecimal getTotalAreaProp() {
         return new BigDecimal("11.0");
     }
 
+    /**
+     * Cham o preço total por bairro
+     *
+     * @return o preço total por bairro
+     */
     public static BigDecimal getTotalPriceByDistrict () {
         return new BigDecimal("61600.00");
     }
 
+    /**
+     * Chama um cômodo
+     *
+     * @return um cômodo
+     */
     public static Room getRoom() {
         Room room = new Room("Quarto", 1.60, 1.20);
 
@@ -36,16 +59,21 @@ public class TestUtilsGenerator {
 
     }
 
+    /**
+     * Chama um bairro
+     *
+     * @return um bairro
+     */
+    public static District getDistrict() {
+        District district = new District("Campeche", new BigDecimal("5600.0"));
+        return district;
+    }
+
     public static Room returnBiggestRoom() {
         Room room = new Room("Cozinha", 4.0, 2.0);
 
         return room;
 
-    }
-
-    public static District getDistrict() {
-        District district = new District("Campeche", new BigDecimal("5600.0"));
-        return district;
     }
 
 
